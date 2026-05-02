947560f096d790102a16a51a093ac3f5b455210a
Siam24857 <siamtechofficial1597@gmail.com>
2026-05-03 00:37:12 +0600

    all is done

--- src/app/lib/auth-client.js
+++ src/app/lib/auth-client.js
@@ -1,7 +1,12 @@
 import { createAuthClient } from "better-auth/react";
 
+const baseURL =
+  typeof window !== "undefined"
+    ? "" // browser → same origin
+    : process.env.BETTER_AUTH_URL; // server → full URL
+
 export const authClient = createAuthClient({
-  baseURL: "/"   
+  baseURL,
 });
 
 export const { signIn, signUp, useSession } = authClient;
